05e84b5bf3c56cf787b3e1b5cbf3d950bb36b726
anvil@stibbsy.co.uk <anvil@stibbsy.co.uk>
2018-02-28 22:06:48 +0000

    Saved automatically

--- forms/MainForm.py
+++ forms/MainForm.py
@@ -4,6 +4,8 @@ import anvil.users
 import tables
 from tables import app_tables
 
+from CCSCompareForm import CCSCompareForm
+
 class MainForm (MainFormTemplate):
   def __init__(self, **properties):
     # You must call self.init_components() before doing anything else in this function
@@ -11,3 +13,9 @@ class MainForm (MainFormTemplate):
 
     # Any code you write here will run when the form opens.
     
+
+  def link_1_click (self, **event_args):
+    # This method is called when the link is clicked
+    self.content_panel.clear()
+    self.content_panel.add_component(CCSCompareForm())
+
