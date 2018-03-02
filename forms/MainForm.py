0702d08a8e4f74b79d3aa4cae8dac5a2769f7852
anvil@stibbsy.co.uk <anvil@stibbsy.co.uk>
2018-03-02 15:41:09 +0000

    Saved version

--- forms/MainForm.py
+++ forms/MainForm.py
@@ -18,21 +18,35 @@ class MainForm (MainFormTemplate):
     if anvil.users.get_user():
       logout_button = Link(text="Logout")
       self.add_component(logout_button, slot="sidebar")
-
-    
+      logout_button.set_event_handler("click", self.lnk_logout_click())
+    else:
+      login_button = Link(text="Login")
+      self.add_component(login_button, slot="sidebar")
+      login_button.set_event_handler("click", self.lnk_login_click())
 
   def link_1_click (self, **event_args):
-    if anvil.users.get_user():
+    if do_login():
       self.content_panel.clear()
       self.content_panel.add_component(CCSCompareForm(), full_width_row = True)
     else:  
+      pass
+ 
+  def do_login(self):
+    if not anvil.users.get_user():
       if anvil.users.login_with_form():
         app_tables.log_logins.add_row(timestamp=datetime.datetime.now(), user=anvil.users.get_user())
-        self.content_panel.clear()
-        self.content_panel.add_component(CCSCompareForm(), full_width_row = True)
+        return True
       else:
-        pass
-
+        return False
+    elif anvil.users.get_user():
+      return True
+        
+      
+  def lnk_login_click (self, **event_args):
+    if confirm("Are you sure you want to log out?"):
+      anvil.users.logout()
+      self.content_panel.clear()
+      self.content_panel.add_component(HomeForm())
   
   def lnk_logout_click (self, **event_args):
     if confirm("Are you sure you want to log out?"):
