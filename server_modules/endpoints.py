ce484af992cf4560f36c1ee3a39763d58ffe9150
anvil@stibbsy.co.uk <anvil@stibbsy.co.uk>
2018-03-06 14:53:23 +0000

    Saved version

--- server_modules/endpoints.py
+++ server_modules/endpoints.py
@@ -11,21 +11,24 @@ from anvil.server import http_endpoint, request
 def check_pending():
   if request.username == anvil.secrets.get_secret('endpoint_username') and request.password == anvil.secrets.get_secret('endpoint_password'):
     waiting_users = app_tables.users.search(confirmed_email=True, enabled=False, admin_notified=False)
-    
-    for user in waiting_users:
-      
-      google.mail.send(to = 'mattstibbs@gmail.com',
-                       subject = "User waiting for authorisation",
-                       text = "{} is waiting for you to enable their account".format(user['email'])
-                      )
-      
-      user['admin_notified'] = True
-      print("Sent admin notification email for {}".format(user['email']))
-    response = anvil.server.HttpResponse(200, "Request successful")
-    return response
+     
+    if len(waiting_users) > 0:
+      for user in waiting_users:   
+        print("{} is waiting".format(user['email']))
+        google.mail.send(to = 'mattstibbs@gmail.com',
+                          subject = "User waiting for authorisation",
+                          text = "{} is waiting for you to enable their account".format(user['email'])
+                        )
+        
+        user['admin_notified'] = True
+        print("Sent admin notification email for {}".format(user['email']))
+      response = anvil.server.HttpResponse(200, "Request successful")
+      return response
+    else:
+      response = anvil.server.HttpResponse(200, "No users waiting")
+      return response
   
   else:
     
     response = anvil.server.HttpResponse(401, "Invalid credentials")
     return response
-    
