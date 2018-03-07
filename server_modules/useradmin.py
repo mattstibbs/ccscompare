b124d4f308e5838fcf6b00a994fb2b2829af47ac
anvil@stibbsy.co.uk <anvil@stibbsy.co.uk>
2018-03-07 16:36:03 +0000

    v

--- server_modules/useradmin.py
+++ server_modules/useradmin.py
@@ -5,6 +5,7 @@ import tables
 from tables import app_tables
 import anvil.secrets
 import anvil.server
+import anvil.http
 
 
 @anvil.server.callable
@@ -30,3 +31,16 @@ def activate_user(u):
       user=u, 
       permissions=[app_tables.permissions.get(code='MENU_CCS_COMPARE')]
     )
+    
+  resp = anvil.http.request(
+      url="https://api.mailgun.net/v3/mg.dos-tools.tech/messages",
+      method="POST",
+      data={"from": "DoS Tools <admin@mg.dos-tools.tech>",
+              "to": "Matt Stibbs <m.stibbs@nhs.net>",
+              "subject": "User activated",
+              "text": "User {} has been activated".format(u['enabled'])
+           },
+      username="api",
+      password=anvil.secrets.get_secret('mailgun_key'))
+  
+  print("response = ",resp)
