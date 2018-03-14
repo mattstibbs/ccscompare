125b7f7ddee0b81af32ab06d4e261c1bc7631b36
anvil@stibbsy.co.uk <anvil@stibbsy.co.uk>
2018-03-14 08:11:55 +0000

    v1.6.22

--- server_modules/file_upload.py
+++ server_modules/file_upload.py
@@ -9,19 +9,14 @@ import csv
 
 @anvil.server.callable
 def import_sgsd_csv(csv_object):
-  print(csv_object)
   # Get the data as bytes.
-  print("Getting bytes...")
   csv_bytes=csv_object.get_bytes()
-  print(csv_bytes)
   # Convert bytes to a string.
-  print("Getting string...")
   csv_string=str(csv_bytes,"utf-8")
-  print(csv_string)
   # Create a list of lines split on \n
-  print("Getting lines...")
   line_list=csv_string.split('\n')
   reader = csv.DictReader(line_list)
+  
   for row in reader:
     app_tables.sg_sd.add_row(sg_code=row['SG'], sg_description=row['SGDescription'], sd_code=row['SD'], sd_description=row['SDDescription'])
   
